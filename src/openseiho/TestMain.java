a7044e8776fd17bdf3caee32da033b6096ce8c18
hidemune <auau@kne.biglobe.ne.jp>
2014-02-23 05:32:14 +0900

    共通関数として新規作成

---
+++ src/openseiho/TestMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package openseiho;
+
+/**
+ *
+ * @author hdm
+ */
+public class TestMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TestFrame frm = new TestFrame();
+        frm.setVisible(true);
+    }
+    
+}

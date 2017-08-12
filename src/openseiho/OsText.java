5e78a0916042f295f8750c6f74b1cd880b922e0c
User <user@localhost.localdomain>
2017-08-12 10:05:15 +0900

    IME Signed-off-by: User <user@localhost.localdomain>

--- src/openseiho/OsText.java
+++ src/openseiho/OsText.java
@@ -23,14 +23,142 @@ package openseiho;
  * @author TANAKA_Hidemune
  */
 public class OsText extends javax.swing.JTextField {
-
+    private int mode = 0;//nanimosinai
+    
     public OsText() {
         super();
+        
+        this.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                OsText.henkan((OsText) e.getSource());
+            }
+        });
     }
     public OsText(String string) {
         super.setText(string);
     }
-    
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+    public int getMode() {
+        return this.mode;
+    }
+    //roma ji kana henkan
+    public static void henkan(OsText Pthis) {
+        String str = Pthis.getText();
+        StringBuilder sb = new StringBuilder();
+        String[][] dic =  {
+            {"a", "あ"},
+            {"i", "い"},
+            {"u", "う"},
+            {"e", "え"},
+            {"o", "お"},
+            {"ka", "か"},
+            {"ki", "き"},
+            {"ku", "く"},
+            {"ke", "け"},
+            {"ko", "こ"},
+            {"sa", "さ"},
+            {"si", "し"},
+            {"su", "す"},
+            {"se", "せ"},
+            {"so", "そ"},
+            {"ta", "た"},
+            {"ti", "ち"},
+            {"tu", "つ"},
+            {"te", "て"},
+            {"to", "と"},
+            {"na", "な"},
+            {"ni", "に"},
+            {"nu", "ぬ"},
+            {"ne", "ね"},
+            {"no", "の"},
+            {"ha", "は"},
+            {"hi", "ひ"},
+            {"hu", "ふ"},
+            {"he", "へ"},
+            {"ho", "ほ"},
+            {"ma", "ま"},
+            {"mi", "み"},
+            {"mu", "む"},
+            {"me", "め"},
+            {"mo", "も"},
+            {"ya", "や"},
+            {"yi", "い"},
+            {"yu", "ゆ"},
+            {"ye", "え"},
+            {"yo", "よ"},
+            {"ra", "ら"},
+            {"ri", "り"},
+            {"ru", "る"},
+            {"re", "れ"},
+            {"ro", "ろ"},
+            {"wa", "わ"},
+            {"wi", "い"},
+            {"wu", "う"},
+            {"we", "え"},
+            {"wo", "を"},
+            {"ga", "が"},
+            {"gi", "ぎ"},
+            {"gu", "ぐ"},
+            {"ge", "げ"},
+            {"go", "ご"},
+            {"za", "ざ"},
+            {"zi", "じ"},
+            {"zu", "ず"},
+            {"ze", "ぜ"},
+            {"zo", "ぞ"},
+            {"da", "だ"},
+            {"di", "ぢ"},
+            {"du", "づ"},
+            {"de", "で"},
+            {"do", "ど"},
+            {"ba", "ば"},
+            {"bi", "び"},
+            {"bu", "ぶ"},
+            {"be", "べ"},
+            {"bo", "ぼ"},
+            {"sya", "しゃ"},
+            {"syi", "しぃ"},
+            {"syu", "しゅ"},
+            {"sye", "しぇ"},
+            {"syo", "しょ"},
+            {"pya", "ぴゃ"},
+            {"pyi", "ぴぃ"},
+            {"pyu", "ぴゅ"},
+            {"pye", "ぴぇ"},
+            {"pyo", "ぴょ"},
+            {"nn", "ん"},
+            {"la", "ぁ"},
+            {"li", "ぃ"},
+            {"lu", "ぅ"},
+            {"le", "ぇ"},
+            {"lo", "ぉ"},
+            {"xa", "ぁ"},
+            {"xi", "ぃ"},
+            {"xu", "ぅ"},
+            {"xe", "ぇ"},
+            {"xo", "ぉ"},
+        };
+        for (int i=0;i<str.length();i++) {
+            String sub = str.substring(i).toLowerCase();
+            int j;
+            for( j=0;j<dic.length;j++) {
+                if (sub.startsWith(dic[j][0])) {
+                    sb.append(dic[j][1]);
+                    i = i + dic[j][0].length() - 1;
+                    break;
+                }
+            }
+            if (sub.charAt(0) == sub.charAt(1)) {
+                sb.append("っ");
+            }else if (j >= dic.length) {
+                //gaitounasi
+                sb.append(sub.charAt(0));
+            }
+        }
+        Pthis.setText(sb.toString());
+    }
     /**
      *
      * @param str
